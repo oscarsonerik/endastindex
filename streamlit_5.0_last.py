238d8ed804ee6b5d2def8cbfa993b0d8cc9224dc
oscarsonerik <92423244+oscarsonerik@users.noreply.github.com>
2024-03-05 09:09:44 +0100

    Update streamlit_5.0_last.py

--- streamlit_5.0_last.py
+++ streamlit_5.0_last.py
@@ -6,10 +6,10 @@ Hello *world!*
 """)
 import numpy as np
 import streamlit as st
-#import geopandas as gpd
+import geopandas as gpd
 import pandas as pd
 import streamlit.components.v1 as stc
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 #import Calculations_streamlit as calc
 import Calculations_streamlit_avg_last as calc_last
